dd702e47c4ab68b3f93a8531c6a2e53c2d229e47
Yoojeong Lee <yjclarelee@postech.ac.kr>
2021-03-31 09:19:31 +0900

    Reroute to page

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 import "./Navigation.css";
 
 function Navigation() {
-    return <div>
+    return <div className="nav">
         <Link to="/">Home</Link>
         <Link to="/about">About</Link>
     </div>
